b647483db4b81824abcde251f7d760022bd486c9
chrisdavidhutchinson <chris.d.hutchinson@live.co.uk>
2022-11-01 12:59:48 +0000

    Change :cohort to user input, added default value when empty input and printed grouped by cohort

--- directory_ex.rb
+++ directory_ex.rb
@@ -1,30 +1,55 @@
 def my_puts(str)
+  # Center output text
   puts str.center(60)
+end
+
+def default_value(str)
+  # Create default value when user value is empty
+  str.empty? ? "BLANK" : str
+end
+
+def question(str)
+  puts "Enter #{str}"
 end
 
 def input_students
   puts "Please enter the details of the students"
   puts "-------------"
+  
   # create an empty array
   students = []
-  puts "Enter name"
   # while the name is not empty, repeat this code
   while true do
-    puts "To finish, just leave blank and hit return"
+    question("name (To finish, enter 'q' and hit return)")
     name = gets.chomp
-    if name.empty?
-      break
-    end
-    puts "Enter cohort"
-    cohort = gets.chomp.to_s
-    puts "Enter hobby"
+      break if name == "q"
+    question("cohort")
+    cohort = gets.chomp
+    question("hobby")
     hobby = gets.chomp
-    puts "Enter country of birth"
+    question("country of birth")
     country = gets.chomp
-    # add the student hash to the array
-    students << {name: name, cohort: cohort, hobby: hobby, country: country}
-    puts "Now we have #{students.count} students" 
-    puts "Enter next name"
+
+    puts "Please review your selections...." 
+    my_puts("Name: #{name}")
+    my_puts("Cohort: #{cohort}")
+    my_puts("Hobby: #{hobby}")
+    my_puts("Country of birth #{country}")
+    puts "Are these correct? Y/N"
+
+    if gets.chomp == "Y"
+      # add the student hash to the array
+      students << {
+        name: default_value(name), 
+        cohort: default_value(cohort).to_sym, 
+        hobby: default_value(hobby), 
+        country: default_value(country)
+      }
+      puts "Now we have #{students.count} students, please enter details for the next student"
+    else
+      puts "Please re-enter the correct details"
+    end
+ 
   end
   # return the array of students
   students
@@ -33,6 +58,17 @@ end
 def print_header
   my_puts("The students of Villains Academy")
   my_puts("-------------")
+end
+
+def print_by_cohort(students)
+  # print students by cohort
+  groups = students.map {|group| group[:cohort]}.uniq
+  
+  groups.each do |group|
+    puts group
+    students.each {|student| puts student[:name] if student[:cohort] == group} 
+  end
+
 end
 
 def print(students)
@@ -81,8 +117,9 @@ end
 # nothing happens until we call the methods
 students = input_students
 print_header
-print(students)
+#print(students)
 # print_while(students)
 # print_with_c(students)
 # print_less_than_twelve(students)
+print_by_cohort(students)
 print_footer(students)
